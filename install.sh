880a31d0b122057f31574bac40887887e74ee2c9
pruebascript <37500184+pruebascript@users.noreply.github.com>
2018-03-18 13:06:45 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,3 @@
-#/bin/bash
-NONE='\033[00m'
-RED='\033[01;31m'
-GREEN='\033[01;32m'
-YELLOW='\033[01;33m'
-PURPLE='\033[01;35m'
-CYAN='\033[01;36m'
-WHITE='\033[01;37m'
-BOLD='\033[1m'
-UNDERLINE='\033[4m'
-MAX=9
-
-COINGITHUB=https://github.com/Bitcoinlightning/Bitcoin-Lightning.git
-COINPORT=17127
-COINRPCPORT=17126
-COINDAEMON=Bitcoin_Lightningd
-COINCORE=.Bitcoin_Lightning
-COINCONFIG=Bitcoin_Lightning.conf
-
 checkForUbuntuVersion() {
    echo "[1/${MAX}] Checking Ubuntu version..."
     if [[ `cat /etc/issue.net`  == *16.04* ]]; then
