36082776d8f052a237f9dac963c08dd3592d67e8
pruebascript <37500184+pruebascript@users.noreply.github.com>
2018-03-18 13:14:34 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-checkForUbuntuVersion() {
-   echo "[1/${MAX}] Checking Ubuntu version..."
-    if [[ `cat /etc/issue.net`  == *16.04* ]]; then
-        echo -e "${GREEN}* You are running `cat /etc/issue.net` . Setup will continue.${NONE}";
-    else
-        echo -e "${RED}* You are not running Ubuntu 16.04.X. You are running `cat /etc/issue.net` ${NONE}";
-        echo && echo "Installation cancelled" && echo;
-        exit;
-    fi
